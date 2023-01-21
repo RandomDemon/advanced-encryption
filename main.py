ccf76b045318c1786506a01d0b8a221202db65e5
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-21 14:18:20 -0500

    TTK Implementation FIX

--- main.py
+++ main.py
@@ -1,4 +1,52 @@
+import sys
+import base64
+from cryptography.fernet import Fernet
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
 from tkinter import ttk
+
+def encrypt_file():
+    password = password_entry.get()
+    if len(password) == 0:
+        messagebox.showerror("Error", "Please enter a password.")
+    else:
+        filepath = filedialog.askopenfilename()
+        if filepath:
+            try:
+                ssb_b64 = base64.b64encode(password.encode())
+                c = Fernet(ssb_b64)
+                with open(filepath, "rb") as f:
+                    data = f.read()
+                    data_c = c.encrypt(data)
+                    with open(filepath+'.encrypted', "wb") as f:
+                        f.write(data_c)
+                    result_label.config(text="File successfully encrypted.")
+            except Exception as e:
+                messagebox.showerror("Error", str(e))
+        else:
+            messagebox.showerror("Error", "Please select a file.")
+
+def decrypt_file():
+    password = password_entry.get()
+    if len(password) == 0:
+        messagebox.showerror("Error", "Please enter a password.")
+    else:
+        filepath = filedialog.askopenfilename()
+        if filepath:
+            try:
+                ssb_b64 = base64.b64encode(password.encode())
+                c = Fernet(ssb_b64)
+                with open(filepath, "rb") as f:
+                    data = f.read()
+                    data_c = c.decrypt(data)
+                    with open(filepath+'.decrypted', "wb") as f:
+                        f.write(data_c)
+                    result_label.config(text="File successfully decrypted.")
+            except Exception as e:
+                messagebox.showerror("Error", str(e))
+        else:
+            messagebox.showerror("Error", "Please select a file.")
 
 root = Tk()
 root.title("Encryption/Decryption GUI")
