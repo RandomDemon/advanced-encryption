3fafbb2be086f5579ac3791b331f3142829235ee
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-21 14:18:44 -0500

    More options

--- main.py
+++ main.py
@@ -19,9 +19,8 @@ def encrypt_file():
                 with open(filepath, "rb") as f:
                     data = f.read()
                     data_c = c.encrypt(data)
-                    with open(filepath+'.encrypted', "wb") as f:
-                        f.write(data_c)
-                    result_label.config(text="File successfully encrypted.")
+                    result_label.config(text=data_c)
+                    save_button.config(state=NORMAL)
             except Exception as e:
                 messagebox.showerror("Error", str(e))
         else:
@@ -40,30 +39,20 @@ def decrypt_file():
                 with open(filepath, "rb") as f:
                     data = f.read()
                     data_c = c.decrypt(data)
-                    with open(filepath+'.decrypted', "wb") as f:
-                        f.write(data_c)
-                    result_label.config(text="File successfully decrypted.")
+                    result_label.config(text=data_c)
+                    save_button.config(state=NORMAL)
             except Exception as e:
                 messagebox.showerror("Error", str(e))
         else:
             messagebox.showerror("Error", "Please select a file.")
 
-root = Tk()
-root.title("Encryption/Decryption GUI")
-
-password_label = ttk.Label(root, text="Enter password:")
-password_label.grid(row=0, column=0)
-
-password_entry = ttk.Entry(root)
-password_entry.grid(row=0, column=1)
-
-encrypt_button = ttk.Button(root, text="Encrypt File", command=encrypt_file)
-encrypt_button.grid(row=1, column=0)
-
-decrypt_button = ttk.Button(root, text="Decrypt File", command=decrypt_file)
-decrypt_button.grid(row=1, column=1)
-
-result_label = ttk.Label(root)
-result_label.grid(row=2, columnspan=2)
-
-root.mainloop()
+def save_file():
+    result = result_label.cget("text")
+    filepath = filedialog.asksaveasfilename()
+    if filepath:
+        with open(filepath, "wb") as f:
+            f.write(result)
+        result_label.config(text="File saved.")
+        save_button.config(state=DISABLED)
+    else:
+        messagebox.
