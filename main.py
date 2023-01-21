b9a1e1d289dd63e891522fd4477fd991f58910e7
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2023-01-21 14:19:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,4 +55,27 @@ def save_file():
         result_label.config(text="File saved.")
         save_button.config(state=DISABLED)
     else:
-        messagebox.
+        messagebox.showerror("Error", "Please select a file.")
+        
+root = Tk()
+root.title("Encryption/Decryption GUI")
+
+password_label = ttk.Label(root, text="Enter password:")
+password_label.grid(row=0, column=0)
+
+password_entry = ttk.Entry(root)
+password_entry.grid(row=0, column=1)
+
+encrypt_button = ttk.Button(root, text="Encrypt File", command=encrypt_file)
+encrypt_button.grid(row=1, column=0)
+
+decrypt_button = ttk.Button(root, text="Decrypt File", command=decrypt_file)
+decrypt_button.grid(row=1, column=1)
+
+result_label = ttk.Label(root, text="")
+result_label.grid(row=2, column=0, columnspan=2)
+
+save_button = ttk.Button(root, text="Save File", command=save_file, state=DISABLED)
+save_button.grid(row=3, column=0, columnspan=2)
+
+root.mainloop()
